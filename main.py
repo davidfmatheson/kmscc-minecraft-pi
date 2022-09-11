aa1ff5b9094a9cf3fe22f5faff455cd2e1c0347a
David Matheson <davidfmatheson@gmail.com>
2022-09-11 12:31:15 -0400

    Starting with hello world

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
 from mcpi.minecraft import Minecraft
-from time import sleep
 
 mc = Minecraft.create()
 
-pos = mc.player.getPos()
-mc.player.setPos(pos.x, pos.y + 100, pos.z)
-
-# while True:
-#     x, y, z = mc.player.getPos()
-#     sleep(0.1)
+pos = mc.postToChat("Hello world!")
